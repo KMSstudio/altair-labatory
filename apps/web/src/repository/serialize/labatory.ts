67d2cbc311bda81df6bb7c5630bdb9d88f930ac6
midpare <midpare@gmail.com>
2026-04-13 22:34:12 +0900

    feat:edit serializeUniversity

--- apps/web/src/repository/serialize/labatory.ts
+++ apps/web/src/repository/serialize/labatory.ts
@@ -13,6 +13,7 @@ export function serializeUniversity(univ: UniversityDbShape): UniversityDTO {
     nameEn: univ.nameEn,
     country: univ.country,
     websiteUrl: univ.websiteUrl,
+    domain: univ.domain,
     createdAt: univ.createdAt.toISOString(),
   };
 }
